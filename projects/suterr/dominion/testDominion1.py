103854b2ea28ca828e924970a43b37ba78116b23
Ryan Suter <rstrojan@gmail.com>
2020-01-15 17:18:26 -0800

    moved scoring from testDominion to testUtility

--- projects/suterr/dominion/testDominion1.py
+++ projects/suterr/dominion/testDominion1.py
@@ -37,17 +37,4 @@ testUtility.playGame(supply, players,trash)
             
 
 #Final score
-dcs=Dominion.cardsummaries(players)
-vp=dcs.loc['VICTORY POINTS']
-vpmax=vp.max()
-winners=[]
-for i in vp.index:
-    if vp.loc[i]==vpmax:
-        winners.append(i)
-if len(winners)>1:
-    winstring= ' and '.join(winners) + ' win!'
-else:
-    winstring = ' '.join([winners[0],'wins!'])
-
-print("\nGAME OVER!!!\n"+winstring+"\n")
-print(dcs)
+testUtility.finalScore(players)
